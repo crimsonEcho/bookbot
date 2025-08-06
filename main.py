e62da5779f1aa1ba2b82365829dff8e3ceef6b6f
crimsonEcho <status.effective@gmail.com>
2025-08-06 05:18:12 -0500

    add character count to stats.py, refactor stats.py and main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from stats import book_string
+from stats import get_book_text
+from stats import book_characters
 
 def main():
-    drain = book_string()
-    size = len(drain)
-    print(f"{size} words found in the document.")
+    word_list = get_book_text()
+    word_count = len(word_list)
+    char_count = book_characters()
+    print(f"{word_count} words found in the document.")
+    print(char_count)
             
 main()
 
--- stats.py
+++ stats.py
@@ -1,14 +1,20 @@
 def get_book_text():
-    with open("books/frankenstein.txt") as my_file:
-        book = my_file.read()
-        return book
+    with open("books/frankenstein.txt") as book:
+        book_text = book.read()
+        return book_text.split()
 
 def book_string():
-    x = get_book_text()
-    return x.split(None)
+    with open("books/frankenstein.txt") as book:
+        words = book.read()
+        return words.lower()
 
 def book_characters():
-    y = get_book_text()
-    return y.lower()
+    count = {}
+    for letter in book_string():
+        if letter in count:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+    return count
 
 book_characters()
